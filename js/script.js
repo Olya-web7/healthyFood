654e45469ea6510cc845123479c02cf439618518
“Olha <shyshova24@gmail.com>
2021-07-04 13:40:12 +0300

    add scroll modal

--- js/script.js
+++ js/script.js
@@ -134,9 +134,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const modalTimerId = setTimeout(openModal, 5000);
 
-    window.addEventListener('scroll', () => {
+    function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
             openModal();
+            window.removeEventListener('scroll', showModalByScroll);
         }
-    });
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
